92c77eb7d4d4d425c1e8f671702c27d437237532
Eddie Gustafsson <eddie.gustafsson@elev.ga.ntig.se>
2020-07-23 03:18:24 +0200

    Fixed userRoutes ReferenceError.

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const projectsRoutes = require('./api/routes/projects');
+const userRoutes = require('./api/routes/user');
 
 mongoose.connect(
     "mongodb+srv://" + process.env.MONGO_ATLAS_USERNAME + ":" +
