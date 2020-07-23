2674f50ac34a70a8971f962efd4b6f7e062298b4
Eddie Gustafsson <eddie.gustafsson@elev.ga.ntig.se>
2020-07-24 01:37:34 +0200

    Created a model for error codes.

---
+++ server/api/models/errorCodeModel.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const errorCodeSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    project_id: {type: mongoose.Schema.Types.ObjectId, ref: 'Project', required: true},
+    code: {type: String, required: true},
+    location: {type: String, required: true},
+    message: {type: String, required: true},
+    description: {type: String, required: true},
+    last_updated_at: {type: Date, required: true},
+    created_at: {type: Date, required: true}
+});
+
+module.exports = mongoose.model('ErrorCode', errorCodeSchema);
