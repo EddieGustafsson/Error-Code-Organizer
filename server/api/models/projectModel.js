42656ad93de3df65a27d53cee1353364a05d8191
Eddie Gustafsson <eddie.gustafsson@elev.ga.ntig.se>
2020-07-28 02:10:42 +0200

    Added last_updated_at and created_at to the projectModel.

--- server/api/models/projectModel.js
+++ server/api/models/projectModel.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const projectSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     title: {type: String, required: true},
-    date: {type: Date, required: true},
     description: {type: String, required: false},
-    type: {type: String, required: false}
+    last_updated_at: {type: Date, required: true},
+    created_at: {type: Date, required: true}
 });
 
 module.exports = mongoose.model('Project', projectSchema);
