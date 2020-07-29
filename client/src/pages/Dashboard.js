ab9bb52c19ec10e099609e41241f253c53946202
Eddie Gustafsson <eddie.gustafsson@elev.ga.ntig.se>
2020-07-30 00:38:02 +0200

    Updated the dashboard placeholder.

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import { Grid, Segment, Header } from 'semantic-ui-react';
 
-function Dashboard() {
-  return (
-    <div>
-      <p>Test</p>
-    </div>
-  );
+class Dashboard extends React.Component {
+  render() {
+    return (
+      <div>
+        <Segment vertical>
+          <Grid>
+              <Grid.Column floated='left' width={5}>
+                  <Header as='h2'>Dashboard</Header>
+              </Grid.Column>
+              <Grid.Column floated='right' width={5}>
+                  
+              </Grid.Column>
+          </Grid>
+        </Segment>
+        <Segment vertical>
+            <br/>
+            <br/>
+            <br/>
+        </Segment>
+      </div>
+    )
+  }
 }
 
 export default Dashboard;
