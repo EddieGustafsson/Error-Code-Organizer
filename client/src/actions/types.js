0fd77b32b862b2ced1460e914c9b5105be7b98ac
Eddie Gustafsson <eddie.gustafsson@edgus.dev>
2020-11-11 01:21:37 +0100

    Added new user actions types.

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -14,3 +14,16 @@ export const GET_ERROR_CODE = 'GET_ERROR_CODE';
 export const CREATE_ERROR_CODE = 'CREATE_ERROR_CODE';
 export const UPDATE_ERROR_CODE = 'UPDATE_ERROR_CODE';
 export const DELETE_ERROR_CODE  = 'DELETE_ERROR_CODE';
+
+// User Auth
+export const USER_LOADING = 'USER_LOADING';
+export const USER_LOADED = 'USER_LOADED';
+export const AUTH_ERROR = 'AUTH_ERROR';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAILED = 'LOGIN_FAILED';
+export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
+export const REGISTER_FAILED = 'REGISTER_FAILED';
+
+// Errors
+export const GET_ERRORS = 'GET_ERRORS';
+export const CLEAR_ERRORS = 'CLEAR_ERRORS';
