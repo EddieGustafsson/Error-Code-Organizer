9701d60ef5a2e50127fa29abd222c0792f1244e2
Eddie Gustafsson <eddie.gustafsson@edgus.dev>
2020-11-11 01:26:15 +0100

    Added spacing to calm my OCD.

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,29 +1,29 @@
 // Project
-export const GET_PROJECTS = 'GET_PROJECTS';
-export const GET_PROJECT = 'GET_PROJECT';
-export const CREATE_PROJECT = 'CREATE_PROJECT';
-export const UPDATE_PROJECT = 'UPDATE_PROJECT';
-export const ARCHIVE_PROJECT = 'ARCHIVE_PROJECT';
-export const EXPORT_PROJECT = 'EXPORT_PROJECT';
-export const DELETE_PROJECT = 'DELETE_PROJECT';
-export const PROJECT_LOADING = 'PROJECT_LOADING';
+export const GET_PROJECTS       = 'GET_PROJECTS';
+export const GET_PROJECT        = 'GET_PROJECT';
+export const CREATE_PROJECT     = 'CREATE_PROJECT';
+export const UPDATE_PROJECT     = 'UPDATE_PROJECT';
+export const ARCHIVE_PROJECT    = 'ARCHIVE_PROJECT';
+export const EXPORT_PROJECT     = 'EXPORT_PROJECT';
+export const DELETE_PROJECT     = 'DELETE_PROJECT';
+export const PROJECT_LOADING    = 'PROJECT_LOADING';
 
 // Error codes
-export const GET_ERROR_CODES = 'GET_ERROR_CODES';
-export const GET_ERROR_CODE = 'GET_ERROR_CODE';
-export const CREATE_ERROR_CODE = 'CREATE_ERROR_CODE';
-export const UPDATE_ERROR_CODE = 'UPDATE_ERROR_CODE';
+export const GET_ERROR_CODES    = 'GET_ERROR_CODES';
+export const GET_ERROR_CODE     = 'GET_ERROR_CODE';
+export const CREATE_ERROR_CODE  = 'CREATE_ERROR_CODE';
+export const UPDATE_ERROR_CODE  = 'UPDATE_ERROR_CODE';
 export const DELETE_ERROR_CODE  = 'DELETE_ERROR_CODE';
 
 // User Auth
-export const USER_LOADING = 'USER_LOADING';
-export const USER_LOADED = 'USER_LOADED';
-export const AUTH_ERROR = 'AUTH_ERROR';
-export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
-export const LOGIN_FAILED = 'LOGIN_FAILED';
-export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
-export const REGISTER_FAILED = 'REGISTER_FAILED';
+export const USER_LOADING       = 'USER_LOADING';
+export const USER_LOADED        = 'USER_LOADED';
+export const AUTH_ERROR         = 'AUTH_ERROR';
+export const LOGIN_SUCCESS      = 'LOGIN_SUCCESS';
+export const LOGIN_FAILED       = 'LOGIN_FAILED';
+export const REGISTER_SUCCESS   = 'REGISTER_SUCCESS';
+export const REGISTER_FAILED    = 'REGISTER_FAILED';
 
 // Errors
-export const GET_ERRORS = 'GET_ERRORS';
-export const CLEAR_ERRORS = 'CLEAR_ERRORS';
+export const GET_ERRORS         = 'GET_ERRORS';
+export const CLEAR_ERRORS       = 'CLEAR_ERRORS';
