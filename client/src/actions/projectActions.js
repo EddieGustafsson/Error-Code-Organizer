ca536eaf836a215a8e67b2ca9cd91132fb1a5c39
Eddie Gustafsson <eddie.gustafsson@edgus.dev>
2020-11-12 23:01:26 +0100

    Added auth token to projectActions req.

--- client/src/actions/projectActions.js
+++ client/src/actions/projectActions.js
@@ -10,67 +10,84 @@ import {
     DELETE_PROJECT,
     PROJECT_LOADING
 } from './types';
+import { tokenConfig } from './authActions';
+import { returnErrors } from './errorActions';
 
-export const getProjects = ()  => dispatch => {
+export const getProjects = ()  => (dispatch, getState) => {
     dispatch(setProjectLoading());
     axios
-        .get(API.project)
+        .get(API.project, tokenConfig(getState))
         .then(res => dispatch({
             type: GET_PROJECTS,
             payload: res.data
-        }));
+        }))
+        .catch(error => 
+            dispatch(returnErrors(error.response.data, error.response.status))
+        );
 }
 
-export const getProject = (id) => dispatch => {
+export const getProject = (id) => (dispatch, getState) => {
     dispatch(setProjectLoading());
     axios
-        .get(API.project + id)
+        .get(API.project + id, tokenConfig(getState))
         .then(res => dispatch({
             type: GET_PROJECT,
             payload: res.data
-        }));
+        }))
+        .catch(error => 
+            dispatch(returnErrors(error.response.data, error.response.status))
+        );
 }
 
-export const createProject = (project) => dispatch => {
+export const createProject = (project) => (dispatch, getState) => {
     axios
-        .post(API.project, project)
+        .post(API.project, project, tokenConfig(getState))
         .then(res => dispatch({
             type: CREATE_PROJECT,
             payload: res.data
-        }));
+        }))
+        .catch(error => 
+            dispatch(returnErrors(error.response.data, error.response.status))
+        );
     dispatch(getProjects());
 }
 
-export const updateProject = (id, project) => dispatch => {
+export const updateProject = (id, project) => (dispatch, getState) => {
     axios
-        .patch(API.project + id, project)
+        .patch(API.project + id, project, tokenConfig(getState))
         .then(res => dispatch({
             type: UPDATE_PROJECT,
             payload: res.data
-        }));
+        }))
+        .catch(error => 
+            dispatch(returnErrors(error.response.data, error.response.status))
+        );
 }
 
-export const archiveProject = (id) => {
+export const archiveProject = (id) => (dispatch, getState) => {
     return {
         type: ARCHIVE_PROJECT,
         payload: id
     }
 }
 
-export const exportProject = (id) => {
+export const exportProject = (id) => (dispatch, getState) => {
     return {
         type: EXPORT_PROJECT,
         payload: id
     }
 }
 
-export const deleteProject = (id) => dispatch => {
+export const deleteProject = (id) => (dispatch, getState) => {
     axios
-        .delete(API.project + id)
+        .delete(API.project + id, tokenConfig(getState))
         .then(res => dispatch({
             type: DELETE_PROJECT,
             payload: res.data
-        }));
+        }))
+        .catch(error => 
+            dispatch(returnErrors(error.response.data, error.response.status))
+        );
 }
 
 export const setProjectLoading = () => {
