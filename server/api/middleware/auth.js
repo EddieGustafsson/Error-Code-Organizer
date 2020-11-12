7244f1407fe63237d8d21daa462fbfb02cf7528c
Eddie Gustafsson <eddie.gustafsson@edgus.dev>
2020-11-12 01:15:58 +0100

    Updated the auth server middleware.

--- server/api/middleware/auth.js
+++ server/api/middleware/auth.js
@@ -1,16 +1,17 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
+    const token = req.header('x-auth-token');
+
+    if(!token) 
+        return res.status(401).json({message: 'No token, authorization denied'});
+
     try {
-        const token = req.headers.authorization.split(" ")[1];
         const decoded = jwt.verify(token, process.env.JWT_KEY);
-        req.userData = decoded;
+
+        req.user = decoded;
         next();
-    } catch (error){
-        return res.status(401).json({
-            error: {
-                message: 'Auth failed'
-            }
-        });
+    } catch (e) {
+        req.status(400).json({ message: 'Tokne is not valid'});
     }
 };
