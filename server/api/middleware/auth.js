72e88efe180a64c780efd75f201b4d4b864d4add
Eddie Gustafsson <eddie.gustafsson@edgus.dev>
2020-11-12 23:01:01 +0100

    Changed the no auh token error message.

--- server/api/middleware/auth.js
+++ server/api/middleware/auth.js
@@ -4,7 +4,7 @@ module.exports = (req, res, next) => {
     const token = req.header('x-auth-token');
 
     if(!token) 
-        return res.status(401).json({message: 'No token, authorization denied'});
+        return res.status(401).json({message: 'No auth token, authorization has been denied for this request.'});
 
     try {
         const decoded = jwt.verify(token, process.env.JWT_KEY);
