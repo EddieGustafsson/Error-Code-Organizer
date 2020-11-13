d041f4f335bac687b72573dfbbc638ac55c59720
Eddie Gustafsson <eddie.gustafsson@edgus.dev>
2020-11-13 02:06:06 +0100

    Fixed issue where the login component appeard during refresh.

--- client/src/components/PrivateRoute.js
+++ client/src/components/PrivateRoute.js
@@ -2,32 +2,53 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import LoadingContainer from '../components/LoadingContainer';
 
 class PrivateRoute extends React.Component {
 
+    state = {
+        isLoading: true
+    }
+
     static propTypes = {
-        isAuthenticated: PropTypes.bool
+        isAuthenticated: PropTypes.bool,
+        isLoading: PropTypes.bool,
+        success: PropTypes.bool
+    }
+
+    componentDidUpdate(prevProps) {
+        const { isLoading } = this.props;
+        if (isLoading !== prevProps.isLoading) {
+            if(!isLoading) {
+                this.setState({ isLoading: false });
+            }
+        }
     }
 
     render() {
-
         const {component: Component, ...rest} = this.props;
 
-        return (
+        if (this.state.isLoading && !this.props.success) {
+            return(
+                <LoadingContainer />
+            );
+        }
 
-            // Show the component only when the user is logged in
-            // Otherwise, redirect the user to /signin page
+        return (
             <Route {...rest} render={props => (
                 this.props.isAuthenticated ?
                     <Component {...props} />
                 : <Redirect to="/auth/login" />
             )} />
         );
+
     }
 }
 
 const mapStateToProps = state => ({
-    isAuthenticated: state.auth.isAuthenticated
+    isAuthenticated: state.auth.isAuthenticated,
+    success: state.auth.success,
+    isLoading: state.auth.isLoading
 });
 
 export default connect(mapStateToProps)(PrivateRoute);
